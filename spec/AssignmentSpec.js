00ea45d3ab8fa79d941e9761f8ff534e49845d54
Manpreet kaur <manpreetmanku26@gmail.com>
2018-05-28 08:15:55 -0500

    initial commit

---
+++ spec/AssignmentSpec.js
@@ -0,0 +1,40 @@
+describe("Shopping Cart", function () {
+    it("should return items with itemID and price", function () {
+        let obj = new ShoppingCart(204, 56.8)
+        expect(obj.items()).toEqual([{
+            itemID: 204,
+            price: 56.8
+        }])
+    });
+
+    it("should not take in more than 5 items", function () {
+        let obj = new ShoppingCart()
+        obj.scan(12, 23.4)
+        obj.scan(345, 45.6)
+        obj.scan(375, 23.5)
+        obj.scan(0864, 65.4)
+        obj.scan(58, 34.5)
+        expect(obj.scan(34, 354)).toEqual("Your cart is full")
+    });
+
+    it("should not remove an item if the cart is already empty", function () {
+        let obj = new ShoppingCart()
+        obj.remove(34, 354)
+        expect(obj.items()).toEqual([])
+    });
+
+    // it("should remove an item", function(){
+    //     let obj = new ShoppingCart()
+    //     obj.scan(345, 45.6)
+    //     obj.scan(0864, 65.4)
+    //     obj.remove(0864, 65.4)
+    //     expect(obj.items()).toEqual([{itemID: 345, price: 45.6}])
+    // });
+
+    it("should return total of the items", function(){
+        let obj = new ShoppingCart()
+        obj.scan(12, 23.4)
+        obj.scan(345, 45.6)
+        expect(obj.total()).toBe(136.8)
+    })
+})
---
+++ src/Assignment.js
@@ -0,0 +1,45 @@
+class ShoppingCart {
+    constructor(itemID, price) {
+        this.item = [];
+        this.product = {};
+        this.product.itemID = itemID;
+        this.product.price = price;
+        this.item.push(this.product)
+        this.percent = 0;
+    }
+    scan(itemID, price) {
+        if (this.item.length < 5) {
+
+            this.product.itemID = itemID;
+            this.product.price = price;
+            this.item.push(this.product)
+        } else {
+            return "Your cart is full"
+        }
+    }
+    remove(product) {
+        if (this.item.length < 1) {
+            return "Your cart is empty"
+        } else {
+            this.item.pop(product)
+        }
+    }
+    // discount(percent) {
+    //     if (this.percent >= this.total() / 2) {
+    //         return this.total = this.total - (this.percent / 100) * this.total
+    //     } else {
+    //         return this.total() * this.percent / 100
+    //     }
+    // }
+    total() {
+        var total = 0;
+        for (var i = 0; i < this.item.length; i++) {
+            total = total + this.item[i].price //- this.discount(this.percent)
+            console.log(this.item[i].price)
+        }
+        return total
+    }
+    items() {
+        return this.item;
+    }
+}
